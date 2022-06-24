50dc7fe79a9b9afe7dde731cc6409ec350a75761
Walter <silvawalteralipio@gmail.com>
2022-06-24 20:39:22 -0300

    aula 5

--- event-plataform/src/App.tsx
+++ event-plataform/src/App.tsx
@@ -4,30 +4,13 @@ import { client } from "./lib/apollo"
 import { Router } from "./Router";
 
 
-
-
-// const  GET_LESSONS_QUERY = gql`
-//   query {
-//     lessons{
-//       id
-//       title
-
-//       teacher{
-//         name
-//       }
-//     }
-//   }
-// `
-
 interface Lesson {
   id: string;
   title: string
 }
 
 function App() {
-  // const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
 
-  // console.log(data)
   return (
 
     <ApolloProvider client={client}>
